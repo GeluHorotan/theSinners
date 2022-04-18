f592864620cc9ea43ff0b196b38618ce0f0751fa
Horotan Gelu <horotangelu17@gmail.com>
2022-04-19 02:53:36 +0300

    Adding breakpoints to sliders so it works good on mobile

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -19,6 +19,23 @@ const Slider = ({ slidesPerView, sliderClass, items }) => {
         spaceBetween={50}
         className={sliderClass}
         modules={[Navigation, Pagination, Scrollbar, A11y]}
+        breakpoints={{
+          // when window width is >= 320px
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          // when window width is >= 480px
+          480: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+          },
+          // when window width is >= 640px
+          640: {
+            slidesPerView: 3,
+            spaceBetween: 140,
+          },
+        }}
         slidesPerView={slidesPerView}
         navigation
         pagination={{ clickable: true }}
