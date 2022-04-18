edd1fc10e0b8f6bae93d7e5034a12744c2546157
Horotan Gelu <horotangelu17@gmail.com>
2022-04-18 19:31:03 +0300

    Slider and slider component used on Teams page

---
+++ src/components/Slider.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+
+/// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import Card from './playerCard/Card';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+const Slider = ({ slidesPerView, sliderClass, items, pagination }) => {
+  return (
+    <div className='slider-wrapper'>
+      <Swiper
+        spaceBetween={50}
+        className={sliderClass}
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        slidesPerView={slidesPerView}
+        navigation
+        pagination={{ pagination } ? { clickable: true } : ''}
+        scrollbar={{ draggable: true }}
+      >
+        <SwiperSlide>
+          <div className='page'>{items[0]}</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          {' '}
+          <div className='page'>{items[1]}</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          {' '}
+          <div className='page'>{items[2]}</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          {' '}
+          <div className='page'>{items[3]}</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          {' '}
+          <div className='page'>{items[4]}</div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Slider;
--- src/pages/Teams.js
+++ src/pages/Teams.js
@@ -1,117 +1,73 @@
 import React from 'react';
 import styled from 'styled-components';
+
+// Components
 import Card from '../components/playerCard/Card';
+import Slider from '../components/Slider';
 
 // Utility
 import { primary } from '../Utility/Colors';
 import '../Utility/dota2heroes.css';
 
-// Players photos
-import Puppey from '../img/playerCards/puppey.png';
-import Sumail from '../img/playerCards/sumail.png';
-import Nisha from '../img/playerCards/nisha.png';
-import Yapzor from '../img/playerCards/yapzor.png';
-import iceiceice from '../img/playerCards/iceiceice.png';
+import { PlayersData } from '../Data/PlayersData';
 
 const Teams = () => {
   return (
-    <>
-      <BackgroundStyle>
-        <Card
-          className='player-card'
-          cardColor='desaturated-red'
-          topText='THE SINNERS'
-          buttonText='READ MORE'
-          age={22}
-          cardType='player'
-          location='Romania'
-          nickname='Rein.'
-          name='Alexandru Butnaru'
-          position='Safelane (1)'
-          signatureHeroes={[
-            'templar_assassin',
-            'skeleton_king_alt1',
-            'monkey_king_alt1',
-          ]}
-          src={Sumail}
-        />
-        <Card
-          className='player-card'
-          cardColor='orange'
-          topText='THE SINNERS'
-          buttonText='READ MORE'
-          age={22}
-          cardType='player'
-          location='Romania'
-          nickname='oxymoron365'
-          name='Gelu Horotan'
-          position='Midlane (2)'
-          signatureHeroes={[
-            'queenofpain_alt1',
-            'nevermore_alt1',
-            'invoker_persona1',
-          ]}
-          src={Nisha}
-        />
-        <Card
-          className='player-card'
-          cardColor='magenta'
-          topText='THE SINNERS'
-          buttonText='READ MORE'
-          age={22}
-          cardType='player'
-          location='Romania'
-          nickname='gnx'
-          name='Ronald Solticzki'
-          position='Offlane (3)'
-          signatureHeroes={['timbersaw', 'tidehunter', 'furion']}
-          src={iceiceice}
-        />
-        <Card
-          className='player-card'
-          cardColor='pink'
-          topText='THE SINNERS'
-          buttonText='READ MORE'
-          age={22}
-          cardType='player'
-          location='Romania'
-          nickname='Yapz0r'
-          name='Yazied Jaradat'
-          position='Soft Support (4)'
-          signatureHeroes={['rubick_alt1', 'earthshaker_alt2', 'enigma']}
-          src={Yapzor}
-        />
-        <Card
-          className='player-card'
-          cardColor='lightblue'
-          topText='THE SINNERS'
-          buttonText='READ MORE'
-          age={22}
-          cardType='player'
-          location='Romania'
-          nickname='Puppey'
-          name='Clement Ivanov'
-          position='Hard Support (5)'
-          signatureHeroes={['chen', 'enigma', 'bane']}
-          src={Puppey}
-        />
-      </BackgroundStyle>
-    </>
+    <BackgroundStyle>
+      <WrapperStyle>
+        <Slider
+          slidesPerView='3'
+          sliderClass='slider'
+          items={PlayersData.map((player, index) => {
+            return (
+              <Card
+                topText={player.topText}
+                cardColor={player.cardColor}
+                cardType={player.cardType}
+                className={player.className}
+                buttonText={player.buttonText}
+                age={player.age}
+                location={player.location}
+                nickname={player.nickname}
+                name={player.name}
+                position={player.position}
+                signatureHeroes={player.signatureHeroes}
+                src={player.src}
+                key={index}
+              />
+            );
+          })}
+        ></Slider>
+      </WrapperStyle>
+    </BackgroundStyle>
   );
 };
 
 const BackgroundStyle = styled.div`
   width: 100%;
-  height: 100vh;
   background: ${primary};
-  display: grid;
-  grid-template-columns: repeat(5, minmax(0, 1fr));
-  padding: 0 4rem;
-  grid-auto-rows: 1fr;
-  gap: 5rem;
+  height: 90vh;
+`;
 
+const WrapperStyle = styled.div`
   .player-card {
     min-height: 100%;
+  }
+  .slider {
+    width: 100%;
+
+    background: ${primary};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .page {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin: 3rem;
+      width: 100%;
+    }
   }
 `;
 
