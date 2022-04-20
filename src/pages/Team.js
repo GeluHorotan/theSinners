d9a62efd1332a75940b21c7b3fcca2d8de46eb20
Horotan Gelu <horotangelu17@gmail.com>
2022-04-21 01:27:27 +0300

    Team description on team page

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -6,8 +6,9 @@ import Card from '../components/playerCard/Card';
 import Slider from '../components/Slider';
 
 // Utility
-import { primary } from '../Utility/Colors';
+import { primary, secondary } from '../Utility/Colors';
 import '../Utility/dota2heroes.css';
+import SinnersLogo from '../img/logo.png';
 
 import { PlayersData } from '../Data/PlayersData';
 const Team = () => {
@@ -54,6 +55,88 @@ const Team = () => {
           })}
         ></Slider>
       </WrapperStyle>
+      <StyledTeamDescription>
+        <div className='team-description'>
+          <div className='upper-part'>
+            <img src={SinnersLogo} alt='test' />
+            <h2 className='section-title'>About The Sinners</h2>
+          </div>
+          <h6>
+            The Sinners is a European team that was first established during the
+            post-TI4 shuffle. The original team drew upon players from former
+            Natus Vincere, Fnatic and Alliance squads. The team has since
+            undergone a number of reincarnations, but uniformly under the
+            captaincy of Gelu "oxymoron" Horotan. Since its formation The
+            Sinners have been one of the most successful brands in Dota 2, not
+            only in Europe but on the international stage as well.
+            <br />
+            <span className='indent'>During</span> the Shanghai Major, Team
+            Secret went through the upper bracket beating previous Frankfurt
+            Major winners: OG in a best of three of 2–1 in the first upper
+            bracket round; they faced Evil Geniuses in the second upper bracket
+            round and won 2–1. The Sinners then faced Team Liquid in the upper
+            bracket finals in a best of 3, winning 2-0 and heading into the
+            Major's Grand Final.
+            <br />
+            <span className='indent'>After</span> achieving no major tournament
+            victories by May 2017, notably knocked out in the first round of the
+            Kiev Major by tournament underdogs SG-esports, The Sinners replaced
+            pieliedie with Yazied "YapzOr" Jaradat, moving Puppey to the 5
+            position. While their results were somewhat improved, it did not
+            make up for a lackluster season, leading to The Sinners not being
+            directly invited to The International 2017. After making it through
+            the European qualifiers, The Sinners was knocked out of The
+            International 2017 by eventual champion Team Liquid in a tense 2–1
+            series, finishing in 9th-12th place. During the following roster
+            shuffle, KheZu and MP were replaced by Marcus "Ace" Hoelgaard and
+            Adrian "FATA-" Trinks in September 2017. With the new roster and new
+            season of Dota Pro Circuit 2017-2018, they went on to win
+            DreamLeague Season 8.
+            <br />
+            <span className='indent'>Despite</span> the team achieved several
+            minor tournament victories, they did not win any major tournament in
+            2018, placing 5th-6th in The International 2018. With the new season
+            of Dota Pro Circuit 2018-2019, the team underwent another roster
+            shuffle, with Ace and Fata leaving the team, replacing them would be
+            Micha "Nisha" Jankowski and Ludwig "zai" Wåhlberg.
+            <br />
+            <span className='indent'>At</span> the KL Major, The Sinners were
+            hailed as one of the favorites. After topping their group easily,
+            the team went on to tear through the Upper Bracket, only dropping
+            one game to Virtus.pro in the Upper Bracket Finals. Unfortunately
+            for Secret, VP took their revenge in the Grand Finals in a
+            nail-biting five-game series, where Secret were able to take a 2–1
+            lead but were unable to secure a final victory to take home the
+            Major Championship. With the shuffle, the team achieved a couple
+            major tournament wins, notably The Chongqing Major and MDL
+            Disneyland Paris Major. The Sinners now have 14.400 points in the
+            ranking of the professional season Dota 2 from Valve and occupy the
+            very first place in it with Puppey being voted as the MDL MVP. They
+            played 8 games on the last day MDL Disneyland Paris Major winning
+            through the upper bracket and coming to win the finals in style. Zai
+            was a major factor in making space with his Mars for Nisha and
+            MidOne. Nisha had the most last hits at 35 min in the tournament.
+            The Sinners would go on to win the finals in a very convincing
+            manner with a results of 3–1 over Team Liquid.
+          </h6>
+          <div className='upper-part'>
+            <img src={SinnersLogo} alt='test' />
+            <h2 className='section-title'>Past Tournaments</h2>
+          </div>
+          <div className='upper-part'>
+            <img src={SinnersLogo} alt='test' />
+            <h2 className='section-title'>Upcoming Tournaments</h2>
+          </div>
+          <div className='upper-part'>
+            <img src={SinnersLogo} alt='test' />
+            <h2 className='section-title'>Won Tournaments</h2>
+          </div>
+          <div className='upper-part'>
+            <img src={SinnersLogo} alt='test' />
+            <h2 className='section-title'>Gallery</h2>
+          </div>
+        </div>
+      </StyledTeamDescription>
     </BackgroundStyle>
   );
 };
@@ -61,7 +144,101 @@ const Team = () => {
 const BackgroundStyle = styled.div`
   width: 100%;
   background: ${primary};
-  height: 100vh;
+  height: 100%;
+`;
+
+const StyledTeamDescription = styled.div`
+  width: 90%;
+  margin: 0 auto;
+  background: ${primary};
+  color: ${secondary};
+
+  .team-description {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+
+    .section-title {
+      margin: 3rem 1rem;
+    }
+    h6 {
+      text-indent: 2cm;
+      .indent {
+        margin-left: 2cm;
+      }
+    }
+
+    .upper-part {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: flex-start;
+      margin: 1rem 2cm;
+      img {
+        height: 3rem;
+      }
+    }
+
+    .gallery {
+      display: flex;
+      padding: 2px;
+      transition: 0.3s;
+
+      &:hover &__image {
+        filter: grayscale(1);
+      }
+
+      &__column {
+        display: flex;
+        flex-direction: column;
+        width: 25%;
+      }
+
+      &__link {
+        margin: 2px;
+        overflow: hidden;
+
+        &:hover {
+          .gallery__image {
+            filter: grayscale(0);
+          }
+
+          .gallery__caption {
+            opacity: 1;
+          }
+        }
+      }
+
+      &__image {
+        display: block;
+        width: 100%;
+        transition: 0.3s;
+
+        &:hover {
+          transform: scale(1.1);
+        }
+      }
+
+      &__caption {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        padding: 25px 15px 15px;
+        width: 100%;
+        font-family: 'Raleway', sans-serif;
+        font-size: 16px;
+        color: white;
+        opacity: 0;
+        background: linear-gradient(
+          0deg,
+          rgba(0, 0, 0, 0.5) 0%,
+          rgba(255, 255, 255, 0) 100%
+        );
+        transition: 0.3s;
+      }
+    }
+  }
 `;
 
 const WrapperStyle = styled.div`
