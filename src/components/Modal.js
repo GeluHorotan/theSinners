7d2b664410538dbec4b352a763a3dae1fb90eaf0
Horotan Gelu <horotangelu17@gmail.com>
2022-04-27 02:32:19 +0300

    Redesigning the table with the game data

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -48,74 +48,6 @@ const Modal = ({ nickname, src, cardColor, name, SinnersLogo, player_id }) => {
     getHeroData();
   }, []);
 
-  // console.log('😂😂😂', heroData[122]);
-
-  // Mapping Variables
-
-  console.log(playerData);
-
-  const getKda = playerData.map((match, index) => (
-    <p>
-      <GiIcons.GiDrippingSword />
-      {match.kills}/{match.deaths}/{match.assists}
-    </p>
-  ));
-
-  const getMatchID = playerData.map((match, index) => (
-    <p> {match.match_id} </p>
-  ));
-  const getHeroID = playerData.map((match, index) => {
-    return match.hero_id;
-  });
-  const getIMG = getHeroID.map((id, index) => {
-    return (
-      <img src={`https://api.opendota.com${heroData[id - 2].img}`} alt='test' />
-    );
-  });
-
-  const getGPM = playerData.map((match, index) => (
-    <p>
-      {' '}
-      <GiIcons.GiGoldBar /> {match.gold_per_min}
-    </p>
-  ));
-  const getXPM = playerData.map((match, index) => (
-    <p>
-      <GiIcons.GiProgression /> {match.xp_per_min}
-    </p>
-  ));
-  const getLH = playerData.map((match, index) => (
-    <p>
-      {' '}
-      <GiIcons.GiFrankensteinCreature /> {match.last_hits}
-    </p>
-  ));
-  const getDuration = playerData.map((match, index) => (
-    <p>
-      <GiIcons.GiClockwork /> {match.duration}
-    </p>
-  ));
-  const getHeroDMG = playerData.map((match, index) => (
-    <p>
-      <GiIcons.GiDrippingSword /> {match.hero_damage}
-    </p>
-  ));
-
-  const getTowerDMG = playerData.map((match, index) => (
-    <p>
-      <GiIcons.GiHeartTower /> {match.tower_damage}
-    </p>
-  ));
-  const getResult = playerData.map((match, index) => (
-    <p>
-      {match.radiant_win ? (
-        <GiIcons.GiHillConquest />
-      ) : (
-        <GiIcons.GiCornerExplosion />
-      )}
-    </p>
-  ));
-
   return createPortal(
     <StyledModal>
       <div className='modal-background'>
@@ -130,55 +62,101 @@ const Modal = ({ nickname, src, cardColor, name, SinnersLogo, player_id }) => {
           </div>
           <div className='header'>
             <h3 className='name'>
-              {/* <img src={`https://api.opendota.com${heroData[0].img}`} alt='' />{' '} */}
               {splittedName[0]} "{nickname}" {splittedName[1]}
             </h3>
           </div>
           <div className='body'>
-            <div className='recent-games'>
-              <div className='match-id'>
-                <h6>Match ID</h6>
-                {getMatchID && getMatchID}
+            <div className='table-box'></div>
+            <div className='table-row table-head' id={cardColor}>
+              <div className='table-cell first-cell'>
+                <h6>GAME ID</h6>
               </div>
-              <div className='hero-id'>
-                <h6>Hero ID</h6>
-                {getIMG}
+              <div className='table-cell'>
+                <h6>HERO</h6>
               </div>
-              <div className='match-result'>{getResult && getResult}</div>
-              <div className='player-kda'>
-                {' '}
+
+              <div className='table-cell'>
+                <h6>ATTRIBUTES</h6>
+              </div>
+              <div className='table-cell'>
+                <h6>RESULT</h6>
+              </div>
+              <div className='table-cell'>
                 <h6>KDA</h6>
-                {getKda && getKda}
               </div>
-              <div className='player-gpm'>
-                {' '}
+              <div className='table-cell'>
                 <h6>GPM</h6>
-                {getGPM && getGPM}
               </div>
-              <div className='player-xpm'>
-                {' '}
+              <div className='table-cell'>
                 <h6>XPM</h6>
-                {getXPM && getXPM}
               </div>
-              <div className='player-dmg'>
-                {' '}
-                <h6>Hero Damage</h6>
-                {getHeroDMG && getHeroDMG}
+              <div className='table-cell'>
+                <h6>HERO DAMAGE</h6>
               </div>
-              <div className='tower-dmg'>
-                {' '}
-                <h6>Building Damage</h6>
-                {getTowerDMG && getTowerDMG}
+              <div className='table-cell'>
+                <h6>BUILDING DAMAGE</h6>
               </div>
-              <div className='player-lh'>
-                <h6>Last Hits</h6>
-                {getLH && getLH}
+              <div className='table-cell'>
+                <h6>LAST HITS</h6>
               </div>
-              <div className='game-duration'>
-                <h6>Game Duration</h6>
-                {getDuration && getDuration}
+              <div className='table-cell'>
+                <h6>GAME DURATION</h6>
               </div>
             </div>
+            {playerData &&
+              playerData.map((match, index) => (
+                <div className='table-row'>
+                  <div className='table-cell first-cell'>{match.match_id}</div>
+
+                  {heroData &&
+                    heroData.map((hero, index) => {
+                      if (hero.id === match.hero_id) {
+                        return (
+                          <>
+                            <div className='table-cell'>
+                              <img
+                                key={index}
+                                src={`https://api.opendota.com${hero.img}`}
+                                alt=''
+                              />
+                            </div>
+                            <div className='table-cell atr'>
+                              <p>{hero.primary_attr.toUpperCase()}</p>
+                              <p>{hero.attack_type}</p>
+                              <p> {hero.roles[0]}</p>
+                            </div>
+                          </>
+                        );
+                      }
+                    })}
+
+                  <div className='table-cell'>
+                    {match.radiant_win ? <h6>WON</h6> : <h6>LOST</h6>}
+                  </div>
+                  <div className='table-cell'>
+                    <GiIcons.GiDrippingSword /> {match.kills}/{match.deaths}/
+                    {match.assists}
+                  </div>
+                  <div className='table-cell'>
+                    <GiIcons.GiGoldBar /> {match.gold_per_min}
+                  </div>
+                  <div className='table-cell'>
+                    <GiIcons.GiProgression /> {match.xp_per_min}
+                  </div>
+                  <div className='table-cell'>
+                    <GiIcons.GiDrippingSword /> {match.hero_damage}
+                  </div>
+                  <div className='table-cell'>
+                    <GiIcons.GiHeartTower /> {match.tower_damage}
+                  </div>
+                  <div className='table-cell'>
+                    <GiIcons.GiFrankensteinCreature /> {match.last_hits}
+                  </div>
+                  <div className='table-cell last-cell'>
+                    <GiIcons.GiClockwork /> {match.duration}
+                  </div>
+                </div>
+              ))}
           </div>
           <div className='footer'></div>
         </div>
@@ -190,21 +168,22 @@ const Modal = ({ nickname, src, cardColor, name, SinnersLogo, player_id }) => {
 
 const StyledModal = styled.div`
   background: rgba(0, 0, 0, 0.5);
+
   width: 100%;
   margin: 0 auto;
-  min-height: 90%;
+  height: 100vh;
   color: ${primary};
   display: flex;
   z-index: 11;
   position: fixed;
   top: 0;
   align-items: center;
-  overflow: scroll;
+
   justify-content: center;
 
   .modal-background {
     width: 100%;
-    height: 100vh;
+    height: 100%;
 
     margin: 0 auto;
     display: flex;
@@ -213,20 +192,17 @@ const StyledModal = styled.div`
 
     .modal-container {
       background: ${secondary};
-      overflow: scroll;
       width: 90%;
-      min-height: 100%;
-
+      height: 100%;
       display: flex;
       flex-direction: column;
       align-items: center;
-
+      overflow: scroll;
       justify-content: center;
 
       .top-container {
         width: 100%;
-        height: auto;
-
+        height: 100%;
         display: flex;
         align-items: flex-end;
 
@@ -238,7 +214,7 @@ const StyledModal = styled.div`
 
         .sinnersLogo {
           justify-content: center;
-          height: 50%;
+          height: 40%;
         }
         h1.team-name {
           font-size: 10rem;
@@ -261,49 +237,60 @@ const StyledModal = styled.div`
   }
   .body {
     width: 100%;
-    min-height: 100%;
+    height: 20rem;
     margin: 0 auto;
     padding: 2rem;
+    display: flex;
+    flex-direction: column;
     color: black;
-    .match-data {
-      display: flex;
-      justify-content: flex-start;
-      align-items: center;
-      gap: 5rem;
-      margin: 1rem 0;
-    }
-    .recent-games {
-      display: flex;
-      gap: 5rem;
-      justify-content: space-between;
-      min-height: 100%;
-      p {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-    }
-    .match-id,
-    .hero-id,
-    .match-result,
-    .player-kda,
-    .player-gpm,
-    .player-xpm,
-    .player-dmg,
-    .tower-dmg,
-    .player-lh,
-    .game-duration {
-      gap: 1rem;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      min-width: auto;
+    img {
+      width: 5rem;
     }
 
-    .hero-id {
-      img {
-        width: 4rem;
+    .table-box {
+      margin: 50px auto;
+    }
+    .table-row {
+      display: table;
+      width: 100%;
+      margin: 5px auto;
+      background: transparent;
+      padding: 0.5rem 0;
+      color: #555;
+
+      box-shadow: 0 1px 4px 0 rgba(0, 0, 50, 0.3);
+
+      &:nth-child(odd) {
+        background: #dfdbd4;
       }
+    }
+
+    .table-cell {
+      display: table-cell;
+      width: 5%;
+      text-align: center;
+      padding: 4px 0;
+      border-right: 1px solid #d6d4d4;
+      vertical-align: middle;
+    }
+
+    .table-head {
+      background: #8665f7;
+      box-shadow: none;
+      color: #fff;
+      font-weight: 600;
+    }
+    .table-head .table-cell {
+      border-right: none;
+    }
+
+    .last-cell {
+      border-right: none;
+    }
+
+    .first-cell {
+      text-align: left;
+      padding-left: 10px;
     }
   }
 `;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,102 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <TableStyle>
+      <div>TABLE TEST</div>
+      <div className='table-box'></div>
+      <div className='table-row table-head'>
+        <div className='table-cell first-cell'>
+          <p>MATCH ID</p>
+        </div>
+        <div className='table-cell'>
+          <p>HERO</p>
+        </div>
+        <div className='table-cell'>
+          <p>KDA</p>
+        </div>
+      </div>
+      <div className='table-row'>
+        <div className='table-cell first-cell'>
+          <p>54654765765</p>
+        </div>
+        <div className='table-cell'>
+          <p>Templar Assassin</p>
+        </div>
+        <div className='table-cell last-cell'>
+          <p>100/14/2</p>
+        </div>
+      </div>
+      <div className='table-row'>
+        <div className='table-cell first-cell'>
+          <p>54654765765</p>
+        </div>
+        <div className='table-cell'>
+          <p>Chaos Knight</p>
+        </div>
+        <div className='table-cell last-cell'>
+          <p>1014231/1241234/12412</p>
+        </div>
+      </div>
+      <div className='table-row'>
+        <div className='table-cell first-cell'>
+          <p>546547657655465476576554654765765</p>
+        </div>
+        <div className='table-cell'>
+          <p>Phantom Lancer suck my cancer</p>
+        </div>
+        <div className='table-cell last-cell'>
+          <p>1234321/34124324/234</p>
+        </div>
+      </div>
+    </TableStyle>
+  );
 };
+
+const TableStyle = styled.div`
+  .table-box {
+    margin: 50px auto;
+  }
+  .table-row {
+    display: table;
+    width: 80%;
+    margin: 10px auto;
+    background: transparent;
+    padding: 12px 0;
+    color: #555;
+    font-size: 13px;
+    box-shadow: 0 1px 4px 0 rgba(0, 0, 50, 0.3);
+  }
+
+  .table-cell {
+    display: table-cell;
+    width: 30%;
+    text-align: center;
+    padding: 4px 0;
+    border-right: 1px solid #d6d4d4;
+    vertical-align: middle;
+  }
+
+  .table-head {
+    background: #8665f7;
+    box-shadow: none;
+    color: #fff;
+    font-weight: 600;
+  }
+
+  .table-head .table-cell {
+    border-right: none;
+  }
+
+  .last-cell {
+    border-right: none;
+  }
+
+  .first-cell {
+    text-align: left;
+    padding-left: 10px;
+  }
+`;
 
 export default About;
