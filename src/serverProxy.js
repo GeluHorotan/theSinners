d2a025d79601e2285e7e09878dc75bd2c5cf865c
Horotan Gelu <horotangelu17@gmail.com>
2022-05-08 16:24:27 +0300

    Allow cors origin policy enabled on deployed site

--- src/serverProxy.js
+++ src/serverProxy.js
@@ -2,7 +2,7 @@ const proxy = require('http-proxy-middleware');
 const steamApiKey = 'D3ECAFCD18BAD344D7E3A0B44D55E53F';
 module.exports = function (app) {
   app.use(
-    proxy(`https://api.steampowered.com`, {
+    proxy(`/IEconDOTA2_570/GetHeroes/v0001/?key=${steamApiKey}`, {
       target: `https://api.steampowered.com`,
       secure: false,
       changeOrigin: true,
