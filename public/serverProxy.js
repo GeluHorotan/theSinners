b3cd7448ee7b84fd62a6424c16122acffadf1f2a
Horotan Gelu <horotangelu17@gmail.com>
2022-05-08 15:50:23 +0300

    Fixing hero bug

--- public/serverProxy.js
+++ public/serverProxy.js
@@ -1,10 +1,8 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const steamApiKey = 'D3ECAFCD18BAD344D7E3A0B44D55E53F';
-
 module.exports = function (app) {
   app.use(
-    proxy('/sers', {
+    proxy('', {
       target: `https://api.steampowered.com`,
       secure: false,
       changeOrigin: true,
