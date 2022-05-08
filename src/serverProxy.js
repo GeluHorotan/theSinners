ab96163f6594e2f7c37ec6bb79163b5f5c17aa3c
Horotan Gelu <horotangelu17@gmail.com>
2022-05-08 16:21:41 +0300

    Hero Bug Fix Attempt

--- src/serverProxy.js
+++ src/serverProxy.js
@@ -2,13 +2,10 @@ const proxy = require('http-proxy-middleware');
 const steamApiKey = 'D3ECAFCD18BAD344D7E3A0B44D55E53F';
 module.exports = function (app) {
   app.use(
-    proxy(
-      `https://thesinners.netlify.app/IEconDOTA2_570/GetHeroes/v0001/?key=D3ECAFCD18BAD344D7E3A0B44D55E53F`,
-      {
-        target: `https://api.steampowered.com`,
-        secure: false,
-        changeOrigin: true,
-      }
-    )
+    proxy(`https://api.steampowered.com`, {
+      target: `https://api.steampowered.com`,
+      secure: false,
+      changeOrigin: true,
+    })
   );
 };
